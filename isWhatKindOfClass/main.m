d23f11c70e73a5682472d441f6ae61b3296ac30b
dojo8888 <dojo8888@gmail.com>
2014-01-20 15:31:36 +0900

    Initial Commit

---
+++ isWhatKindOfClass/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  isWhatKindOfClass
+//
+//  Created by KangNamgyu on 2014. 1. 20..
+//  Copyright (c) 2014년 KangNamgyu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
